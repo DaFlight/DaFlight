ecebcf62cee6a5d970720325bdc90263934e9a68
dags- <dags@dags.me>
2019-12-20 16:17:32 +0000

    Close, no cigar

--- src/main/java/me/dags/daflight/Launch.java
+++ src/main/java/me/dags/daflight/Launch.java
@@ -1,5 +1,6 @@
 package me.dags.daflight;
 
+import org.spongepowered.asm.mixin.MixinEnvironment;
 import org.spongepowered.asm.mixin.Mixins;
 import org.spongepowered.asm.mixin.connect.IMixinConnector;
 
@@ -7,5 +8,6 @@ public class Launch implements IMixinConnector {
     @Override
     public void connect() {
         Mixins.addConfiguration("mixin.daflight.json");
+        MixinEnvironment.getCurrentEnvironment().setObfuscationContext("searge");
     }
 }
